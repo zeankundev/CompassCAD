14559c46769d35066065ebceaf6897f0792a6228
zeankun.dev <zeanfender11@gmail.com>
2024-09-01 18:05:39 +0700

    updated icons to a fresh new one

--- src/js/WindowHandling.js
+++ src/js/WindowHandling.js
@@ -1,6 +1,16 @@
 const remote = require('@electron/remote')
 
 const getWin = () => remote.BrowserWindow.getFocusedWindow()
+var lastWindowState = 'win'
 document.getElementById('minimize').onclick = () => getWin().minimize()
 document.getElementById('maximize').onclick = () => getWin().isMaximized() ? getWin().unmaximize() : getWin().maximize()
 document.getElementById('close').onclick = () => getWin().close()
+setInterval(function () {
+    if (getWin().isMaximized() & lastWindowState == 'win') {
+        lastWindowState = 'max'
+        document.getElementById("maximize").innerHTML = `<img src="../../assets/icons/restoredown.svg">`
+    } else if (!getWin().isMaximized() & lastWindowState == 'max') {
+        lastWindowState = 'win'
+        document.getElementById("maximize").innerHTML = `<img src="../../assets/icons/maximize.svg">`
+    }
+}, 0)
