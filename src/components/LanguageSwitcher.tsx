fa7dd2dfb36f37f23b80fc9382eb440957fce73e
zeankun <zeanfender11@gmail.com>
2025-07-14 13:40:06 +0700

    refix commit #2

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -2,24 +2,9 @@ import React from "react";
 import { GetLanguage, locales } from "./LanguageHandler";
 import styles from '../styles/home.module.css'
 const LanguageSwitcher = () => {
-     function getFlagEmoji(countryCode: string) {
-        if (!countryCode || countryCode.length !== 2) {
-            return ''; // Or handle invalid input as you see fit
-        }
-
-        const codePoints = countryCode
-            .toUpperCase()
-            .split('')
-            .map(char => 127397 + char.charCodeAt(0)); // 127397 is the offset for regional indicator symbols
-
-        return String.fromCodePoint(...codePoints);
-        }
     return (
         <React.Fragment>
             <div className={styles['language-switcher-preview']}>
-                <img
-                    src={`https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/${getFlagEmoji(GetLanguage())}`}
-                />
                 {GetLanguage().toUpperCase()}
             </div>
         </React.Fragment>
