d5b8b45ad74534addef9c8f61d110cecab918a32
zeankun <zeanfender11@gmail.com>
2025-07-14 14:00:16 +0700

    fixed classing and more UX for the switcher

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,13 +1,37 @@
-import React from "react";
-import { GetLanguage, locales } from "./LanguageHandler";
+import React, { useState } from "react";
+import { GetLanguage, SetLanguage, supportedLanguages } from "./LanguageHandler";
 import styles from '../styles/home.module.css'
+import ReactCountryFlag from "react-country-flag";
 const LanguageSwitcher = () => {
+    const [languagePicker, showLanguagePicker] = useState<boolean>(false);
     return (
-        <React.Fragment>
-            <div className={styles['language-switcher-preview']}>
+        <div>
+            <div 
+                className={styles['language-switcher-preview']}
+                onClick={() => showLanguagePicker(!languagePicker)}
+            >
+                <ReactCountryFlag countryCode={
+                    GetLanguage() === 'en' ? 'us' : GetLanguage()
+                } svg />
                 {GetLanguage().toUpperCase()}
             </div>
-        </React.Fragment>
+            {languagePicker && (
+                <div className={styles['language-switcher-dropdown']}>
+                    {supportedLanguages.map((language, index) => (
+                        <div 
+                            className={`${styles['language-dropdown-choice']} ${GetLanguage() == language ? styles['language-active']: ''} ${index == supportedLanguages.length - 1 ? styles.end : ''}`}
+                            key={index}
+                            onClick={() => {SetLanguage(language);showLanguagePicker(false);window.location.reload()}}
+                        >
+                            <ReactCountryFlag countryCode={
+                                language === 'en' ? 'us' : language
+                            } svg />
+                            {language}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
     )
 }
 export default LanguageSwitcher;
