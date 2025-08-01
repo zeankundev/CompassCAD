170caf030e26a4f139bd860b590ae143e0be5a08
zeankun <zeanfender11@gmail.com>
2025-08-01 09:36:46 +0700

    edited to wipe out firestore

--- src/pages/CommunitySignIn.tsx
+++ src/pages/CommunitySignIn.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import { signInWithGoogle, signInWithEmailPassword, createUserIfNotExists } from '../components/FirebaseConfig';
 import styles from '../styles/community.module.css';
 
 export const CommunitySignIn = () => {
     return (
         <div className={styles['community-signin']}>
             <h1>Community Sign In</h1>
-            
+
         </div>
     );
 };
