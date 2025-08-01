2eb1990322c20ff9071b2cba00deec0b242e0aa5
zeankun <zeanfender11@gmail.com>
2025-08-01 10:25:18 +0700

    deleted community

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import Editor from './pages/Editor';
 import EditorHome from './pages/EditorHome';
 import DownloadPage from './pages/Download';
 import { GetLanguage } from './components/LanguageHandler';
-import { CommunitySignIn } from './pages/CommunitySignIn';
-import { CommunityHome } from './pages/CommunityHome';
 
 function App() {
     useEffect(() => {
@@ -24,8 +22,6 @@ function App() {
                     <Route path='/download' component={DownloadPage} />
                     <Route path='/editor/:id' component={Editor} />
                     <Route path='/editor' component={EditorHome} />
-                    <Route path='/community/signin' component={CommunitySignIn} />
-                    <Route exact path='/community' component={CommunityHome} />
                 </Switch>
             </Router>
         </div>
